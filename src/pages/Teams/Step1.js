eb264bf366bfff966eb576ad3a584080da66bf8d
Jonel <it.jonelfignacio@gmail.com>
2022-04-21 13:57:31 +0800

    Add multi step form using useState

--- src/pages/Teams/Step1.js
+++ src/pages/Teams/Step1.js
@@ -1,16 +1,33 @@
 import { FormHelperText, FormLabel, Input } from "@chakra-ui/react";
 import React from "react";
 
-const Step1 = () => {
+const Step1 = ({ formState, setFormState }) => {
+  const { firstName, lastName } = formState;
   return (
     <>
       <FormLabel htmlFor="firstName">Firts Name:</FormLabel>
-      <Input type="text" id="firstName" placeholder="Enter your firt name..." />
+      <Input
+        type="text"
+        id="firstName"
+        placeholder="Enter your firt name..."
+        defaultValue={firstName}
+        onChange={(e) =>
+          setFormState({ ...formState, firstName: e.target.value })
+        }
+      />
       <FormHelperText>Error's show here!</FormHelperText>
       <FormLabel htmlFor="firstName" mt={3}>
         Last Name:
       </FormLabel>
-      <Input type="text" id="lastName" placeholder="Enter your last name..." />
+      <Input
+        type="text"
+        id="lastName"
+        placeholder="Enter your last name..."
+        defaultValue={lastName}
+        onChange={(e) =>
+          setFormState({ ...formState, lastName: e.target.value })
+        }
+      />
       <FormHelperText>Error's show here!</FormHelperText>
     </>
   );
--- src/pages/Teams/Step3.js
+++ src/pages/Teams/Step3.js
@@ -1,22 +1,53 @@
 import React, { useState } from "react";
 import { Checkbox, FormHelperText, FormLabel, Select } from "@chakra-ui/react";
 
-const Step3 = () => {
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../App.css";
+
+const Step3 = ({ formState, setFormState }) => {
+  const { teamGroup, datePicker, compliance } = formState;
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <>
       <FormLabel htmlFor="teamGroup">Team Group:</FormLabel>
-      <Select>
+      <Select
+        id={"teamGroup"}
+        defaultValue={teamGroup}
+        onChange={(e) =>
+          setFormState({ ...formState, teamGroup: e.target.value })
+        }
+      >
         <option hidden>Select Team</option>
-        <option value="team red">Team Red</option>
-        <option value="team blue">Team Blue</option>
-        <option value="team green">Team Green</option>
+        <option value="Team Red" id="teamRed">
+          Team Red
+        </option>
+        <option value="Team Blue" id="teamBlue">
+          Team Blue
+        </option>
+        <option value="Team Green" id="teamGreen">
+          Team Green
+        </option>
       </Select>
       <FormHelperText>Error's show here!</FormHelperText>
+      <FormLabel mt={3}>Date:</FormLabel>
+      <DatePicker
+        id="datePicker"
+        className="datePicker"
+        selected={datePicker == "" ? startDate : datePicker}
+        onChange={(date: Date) => {
+          setStartDate(date);
+          setFormState({ ...formState, datePicker: date });
+        }}
+      />
+      <FormHelperText>Error's show here!</FormHelperText>
 
-      <FormLabel htmlFor="compliance" mt={3}>
-        Compliance:
-      </FormLabel>
-      <Checkbox id="compliance">
+      <FormLabel mt={3}>Compliance:</FormLabel>
+      <Checkbox
+        id="compliance"
+        defaultChecked={compliance}
+        onChange={() => setFormState({ ...formState, compliance: !compliance })}
+      >
         I hereby agree to store and use my personal information
       </Checkbox>
       <FormHelperText>Error's show here!</FormHelperText>
