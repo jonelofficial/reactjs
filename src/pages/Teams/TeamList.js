32dc401fd5a887fdabc3728edd20034442370fa0
Jonel <it.jonelfignacio@gmail.com>
2022-04-22 16:50:23 +0800

    Add chakra ui table

--- src/pages/Teams/TeamList.js
+++ src/pages/Teams/TeamList.js
@@ -1,6 +1,19 @@
-import { Box, Spinner } from "@chakra-ui/react";
-import React from "react";
-import { QueryClientProvider, QueryClient, useQuery } from "react-query";
+import {
+  Box,
+  Spinner,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+} from "@chakra-ui/react";
+import React, { useMemo } from "react";
+import { useQuery } from "react-query";
+import { useTable } from "react-table/dist/react-table.development";
 import { getTeams } from "../../api/TeamsAPI";
 
 const TeamList = () => {
@@ -10,10 +23,55 @@ const TeamList = () => {
   if (isError) return <Box>Error</Box>;
 
   return (
-    <Box>
-      {data.map(({ firstName }, index) => (
-        <Box key={index}>{firstName}</Box>
-      ))}
+    <Box boxShadow="lg" borderRadius="lg" w="container.lg" m="auto" mt={10}>
+      <TableContainer>
+        <Table variant="striped" size="sm">
+          <TableCaption>Team Details</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>First Name</Th>
+              <Th>Last Name</Th>
+              <Th>Email</Th>
+              <Th>Phone Number</Th>
+              <Th>Team Group</Th>
+              <Th>Date Created</Th>
+              <Th textAlign="center">Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map(
+              (
+                {
+                  firstName,
+                  lastName,
+                  email,
+                  phoneNumber,
+                  teamGroup,
+                  datePicker,
+                },
+                index
+              ) => (
+                <Tr key={index}>
+                  <Td>{firstName}</Td>
+                  <Td>{lastName}</Td>
+                  <Td>{email}</Td>
+                  <Td>{phoneNumber}</Td>
+                  <Td>{teamGroup}</Td>
+                  <Td>{datePicker}</Td>
+                  <Td textAlign="center">
+                    <Button colorScheme="teal" size="sm" mr={3}>
+                      Edit
+                    </Button>
+                    <Button colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </Td>
+                </Tr>
+              )
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
